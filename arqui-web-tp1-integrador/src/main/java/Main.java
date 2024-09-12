95c7b2cada1f3bbbe1015086cf6dd618515f59f8
Santiago López de Armentia <santiago_lopez@edsa.com.ar>
2024-09-11 21:56:03 -0300

    mensaje de ayudas para el manejo de las variables booleandas

--- arqui-web-tp1-integrador/src/main/java/Main.java
+++ arqui-web-tp1-integrador/src/main/java/Main.java
@@ -10,12 +10,17 @@ import java.sql.ResultSet;
 public class Main {
     public static void main(String[] args) {
 
+        /* Para poder hacer pruebas en otro ambiente,
+         * debera cambiarse el valor de la ruta
+         */
         final String ROOT_PROJECT = "./ArquiWeb-Grupo9-TPE/arqui-web-tp1-integrador/datasets/";
         final String NB_FILE_CLIENTES = "arquiweb_clientes.csv";
         final String NB_FILE_FACTURAS = "arquiweb_facturas.csv";
         final String NB_FILE_PRODUCTOS = "arquiweb_productos.csv";
         final String NB_FILE_FACTURAS_PRODUCTOS = "arquiweb_facturas-productos.csv";
+        /* Se utiliza para manejar la creacion de las relaciones entre las tablas */
         boolean generateRelations = false;
+        /* Se utiliza para manejar la carga de datos a la base de datos */
         boolean loadData = false;
 
         MySqlFactory mysql = MySqlFactory.getInstance();
@@ -39,6 +44,13 @@ public class Main {
             facturaProducto.createTable();
 
             System.out.println("3. #### GENERAR RELACIONES");
+            /*
+             * Generar relaciones entre tablas.
+             *
+             * Se hace uso de una variable booleana,
+             * la cual debe cambiarse el valor en la declaracion de variables [generateRelations]
+             * para poder manejar en las pruebas en la creacion de relaciones entre tablas.
+             */
             if (generateRelations) {
                 mysql.relacionarTablaUnoMuchos("factura", "idCliente", "cliente", "idCliente",
                         "RELACION_CLIENTE_FACTURA");
@@ -48,6 +60,14 @@ public class Main {
                         "RELACION_FACTURAPRODUCTO_PRODUCTO");
             }
             System.out.println("4. ##### GENERAR INSERTAR DATOS");
+
+            /*
+             * Servicio de carga de datos a la base de datos.
+             *
+             * Se hace uso de una variable booleana,
+             * la cual debe cambiarse el valor en la declaracion de variables [loadData]
+             * para poder manejar en las pruebas la carga de datos.
+             */
 
             Servicio servicio = new Servicio();
             if (loadData) {
@@ -59,6 +79,10 @@ public class Main {
                         Paths.get(ROOT_PROJECT, NB_FILE_FACTURAS_PRODUCTOS).toString());
             }
 
+            /* Servicio 3
+             *   Ejecucion del servicio 3 y muestra de resultados.
+             * */
+
             ResultSet rs_serv3 = servicio.servicio3(mysql.getConnection());
             while (rs_serv3.next()) {
                 System.out.println("id: " + rs_serv3.getInt("idProducto") +
@@ -66,6 +90,9 @@ public class Main {
                         " costo: " + rs_serv3.getFloat("valor"));
             }
 
+            /* Servicio 4
+            *   Ejecucion del servicio 4 y muestra de resultados.
+            * */
             ResultSet rs_serv4 = servicio.servicio4(mysql.getConnection());
             while (rs_serv4.next()) {
                 System.out.println("Cliente: [ idCliente: " + rs_serv4.getInt("idCliente") +
